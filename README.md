659b824684e26c4e4e011f7bd579a17d92eae96e
mac-yuuichi <orangeeeee.ore.ore@gmail.com>
2024-08-26 21:33:30 +0900

    メモ

---
+++ README.md
@@ -0,0 +1,40 @@
+# 技術メモ
+## UnitTest
+### Kotest
+httpapiテスト
+Stoveライブラリ　
+
+### mockitoの話
+
+## aws
+### terraform
+atlantisコマンド
+
+## SNS
+
+## SQS
+
+## StepFunctions
+
+## ECS
+
+## JPA
+JAPはSpring Data JAPを使用する<br/>
+https://spring.io/projects/spring-data-jpa
+
+```
+<!-- Spring Data JPA -->
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-data-jpa</artifactId>
+</dependency>
+```
+
+参考サイトは本家が一番良さそう。
+ここを読めば大体理解できそう。
+https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+
+
+
+## ドメイン駆動
+ファーストクラスコレクション(結合が強いし、DBというより概念モデル)
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.jetbrains.kotlin.plugin.jpa") version "2.0.20"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
 }
-
 group = "com.kt"
 version = "0.0.1-SNAPSHOT"
 
@@ -28,7 +28,11 @@ dependencies {
     val awsSdkVersion = "2.20.135"
     implementation("software.amazon.awssdk:sts:$awsSdkVersion")
     implementation("software.amazon.awssdk:s3:$awsSdkVersion")
+    implementation("software.amazon.awssdk:sfn:$awsSdkVersion")
+    implementation("software.amazon.awssdk:sns:$awsSdkVersion")
+    implementation("software.amazon.awssdk:arns:$awsSdkVersion")
     implementation("software.amazon.awssdk:cognitoidentityprovider:$awsSdkVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -38,6 +42,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
